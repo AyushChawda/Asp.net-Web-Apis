e9b8d82bec9e75c0680ce80fa6f625a9a82cc5f6
Ayush <ayushchawda123@gmail.com>
2025-06-25 00:28:02 +0530

    DOTs practice

---
+++ Models/StudentDOTs.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_APIS.Models
+{
+    public class StudentDOTs
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        //public int age { get; set; }
+        public int standrad { get; set; }
+    }
+}

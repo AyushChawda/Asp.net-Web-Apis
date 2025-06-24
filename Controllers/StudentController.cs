ddc7fc6a7f0e7cc0a1c84313e8d7b8a383b5ede2
Ayush <ayushchawda123@gmail.com>
2025-06-24 15:53:42 +0530

    Create Api

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 using Web_APIS.Models;
+using System.Collections.Generic;
+
 
 namespace Web_APIS.Controllers
 {
@@ -19,6 +22,26 @@ namespace Web_APIS.Controllers
         {
 
             return StudentRepository.Students.Where(x => x.id == id).FirstOrDefault();
+        }
+
+        [HttpGet("{name:alpha}")]
+        public StudentModel getStudentByName(string name)
+        {
+
+            return StudentRepository.Students.Where(x => x.name == name).FirstOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+
+        public bool DeleteStudent(int id)
+        {
+            var student = StudentRepository.Students.Where(x => x.id == id).FirstOrDefault();
+            if (student!=null)
+            {
+                StudentRepository.Students.Remove(student);
+            }
+            
+            return true;
         }
     }
 }
